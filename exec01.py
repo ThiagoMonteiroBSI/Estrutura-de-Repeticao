211e999f0f798bff441937a2ab6727623c829aae
Thiago de Paula Monteiro <thiagodepaulamonteiro@gmail.com>
2025-03-25 13:58:53 -0300

    exec1

---
+++ exec01.py
@@ -0,0 +1,12 @@
+#faça um algoritimo que leia 20 numeros inteiros, escreva os numeros que são negativos e escreva a média dos positivos
+
+numero = 0
+soma = 0
+cont = 0
+
+while numero !=20:
+    numero = int(input("write a number: "))
+    if numero < 0:
+        soma = soma + numero
+        cont = cont +1
+print(soma / cont)
